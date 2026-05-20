cddc7ebdb9c097cc5c6ddf45c0d15a3e67c9caa7
eshtiak23 <eshtiak4099@gmail.com>
2026-05-20 12:35:17 +0600

    fix: update CORS origin to support multiple domains

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ideavault-server-nine.vercel.app/"
+    ],
     credentials: true,
   })
 );
