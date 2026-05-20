32127be2e648b31a7746df8385292d4f15f6a57d
eshtiak23 <eshtiak4099@gmail.com>
2026-05-20 10:20:52 +0600

    Refactor index.js to enhance code clarity by removing unnecessary comments and organizing middleware and route definitions

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// mongodb uri
 const uri = process.env.MONGO_URI;
 
-// mongodb client
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,43 +21,33 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-
   try {
-
-    // connect mongodb
     await client.connect();
-
     console.log("MongoDB Connected Successfully");
 
-    // database
     const database = client.db("ideaVaultDB");
-
-    // collection
     const ideasCollection = database.collection("ideas");
 
-
-
-
-    // GET ALL IDEAS
-
     app.get("/ideas", async (req, res) => {
-
       const result = await ideasCollection
         .find()
         .sort({ createdAt: -1 })
         .toArray();
 
       res.send(result);
-
     });
 
+    app.get("/ideas/:id", async (req, res) => {
+      const id = req.params.id;
 
+      const result = await ideasCollection.findOne({
+        _id: new ObjectId(id),
+      });
 
-
-    // GET TRENDING IDEAS
+      res.send(result);
+    });
 
     app.get("/trending-ideas", async (req, res) => {
-
       const result = await ideasCollection
         .find()
         .sort({ createdAt: -1 })
@@ -68,41 +55,25 @@ async function run() {
         .toArray();
 
       res.send(result);
-
     });
 
-
-
-
-    // ADD IDEA
-
     app.post("/ideas", async (req, res) => {
-
       const idea = req.body;
-
       idea.createdAt = new Date();
 
       const result = await ideasCollection.insertOne(idea);
-
       res.send(result);
-
     });
 
-
-
-    // root route
     app.get("/", (req, res) => {
       res.send("IdeaVault server is running");
     });
-
   } finally {
-
   }
 }
 
 run().catch(console.dir);
 
-// server
 app.listen(port, () => {
   console.log(`IdeaVault server running on port ${port}`);
 });
